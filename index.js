48f6fb87b6e3f7df99e5c1c90d1a58852157edfc
Gabriel <hernandez.raymond.gabriel@gmail.com>
2023-09-04 03:27:34 -0700

    Whole proj
    i gotta keep up with this, did js, and css on one go

---
+++ index.js
@@ -0,0 +1,40 @@
+let shufButton = document.getElementById("shuffle-btn");
+
+
+
+function shuffleArray(array) {
+
+    for (let i = array.length - 1; i > 0; --i) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    return array;
+
+}
+
+function displayShuffle() {
+
+
+    let array = ["Gabe", "Gisselle", "Mary", "Matthew", "Janie", "Allie", "Kassy", "Lanna"];
+    var shuffleResult = shuffleArray(array);
+
+
+    var seat1 = document.getElementById("s0").innerHTML = shuffleResult[0];
+    var seat2 = document.getElementById("s1").innerHTML = shuffleResult[1];
+    var seat3 = document.getElementById("s2").innerHTML = shuffleResult[2];
+    var seat4 = document.getElementById("s3").innerHTML = shuffleResult[3];
+    var seat5 = document.getElementById("s4").innerHTML = shuffleResult[4];
+    var seat6 = document.getElementById("s5").innerHTML = shuffleResult[5];
+    var seat7 = document.getElementById("s6").innerHTML = shuffleResult[6];
+    var seat8 = document.getElementById("s7").innerHTML = shuffleResult[7];
+
+
+    // console.log(shuffleResult);
+
+
+
+    return shuffleResult;
+}
